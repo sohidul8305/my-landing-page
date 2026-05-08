b51fd50fc22f5ea2c5c444389ec35756219dd2ed
Arafat Hossin <si616132@gmail.com>
2026-05-08 17:26:30 +0600

    Complete landing page

--- script.js
+++ script.js
@@ -2,6 +2,32 @@ const mainBtn = document.getElementById("mainBtn");
 
 mainBtn.addEventListener("click", function(){
 
-    alert("Welcome To Modern UI Website 🚀");
+    alert("Welcome To Modern Website 🚀");
 
 });
+
+/* SCROLL REVEAL */
+
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+
+    const reveals = document.querySelectorAll(".reveal");
+
+    for(let i = 0; i < reveals.length; i++){
+
+        const windowHeight = window.innerHeight;
+
+        const revealTop = reveals[i].getBoundingClientRect().top;
+
+        const revealPoint = 100;
+
+        if(revealTop < windowHeight - revealPoint){
+
+            reveals[i].classList.add("active");
+
+        }
+
+    }
+
+}
